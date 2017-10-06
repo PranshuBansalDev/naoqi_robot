9585d4cae1ed8062c51762631bb6fb28c64e7193
prbans <prbans@microsoft.com>
2017-10-05 20:30:21 -0700

    fixed it recognizing its own voice

--- facerecognition.py
+++ facerecognition.py
@@ -34,7 +34,7 @@ class HumanGreeter(object):
         # init asr_service
         self.speech_to_text.pause(True)
         self.speech_to_text.setLanguage("English")
-        self.vocab = ["yes", "no", "please"]
+        self.vocab = ["pranshu", "rolly", "esin"]
         self.speech_to_text.setVocabulary(self.vocab, False)
 
         # Add subscription for face detection
@@ -51,18 +51,15 @@ class HumanGreeter(object):
 
         # Subscribe to the face_detection
         self.face_detection.subscribe("HumanGreeter")
+        self.speech_to_text.setAudioExpression(True)
         self.speech_to_text.pause(False)
 
     def on_word_recognized(self, value):
         print value
-        if value[1] > 0.5:
-            self.tts.say("You just said {}".format(value[0]))
-        # if value == []:
-        #    self.got_word = False
-        #elif not self.got_word:
-        #    self.got_word = True
-        #    self.last_word_detected = time.time()
-        #    print value
+        if value[1] > 0.35:
+            self.speech_to_text.pause(True)
+            self.tts.say("Hi {}".format(value[0]))
+            self.speech_to_text.pause(False)
 
     def on_human_tracked(self, value):
         """
@@ -73,28 +70,8 @@ class HumanGreeter(object):
         elif not self.got_face and (time.time() - self.last_face_detected >= 10):  # only speak the first time a face appears
             self.got_face = True
             print "I saw a face!"
-            self.tts.say("Hello, you!")
+            self.tts.say("Hello! what is your name?")
             self.last_face_detected = time.time()
-            """
-            # First Field = TimeStamp.
-            timeStamp = value[0]
-            print "TimeStamp is: " + str(timeStamp)
-
-            # Second Field = array of face_Info's.
-            face_info_array = value[1]
-            for j in range(len(face_info_array) - 1):
-                face_info = face_info_array[j]
-
-                # First Field = Shape info.
-                face_shape_info = face_info[0]
-
-                # Second Field = Extra info (empty for now).
-                face_extra_info = face_info[1]
-
-                print "Face Infos :  alpha %.3f - beta %.3f" % (face_shape_info[1], face_shape_info[2])
-                print "Face Infos :  width %.3f - height %.3f" % (face_shape_info[3], face_shape_info[4])
-                print "Face Extra Infos :" + str(face_extra_info)
-        """
 
     def run(self):
         """
